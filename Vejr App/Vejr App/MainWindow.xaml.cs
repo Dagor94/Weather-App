16fa4a30316ef1f70b3d7de6da2a87821b6e7319
Dagor94 <Philler9460@gmail.com>
2023-06-21 22:34:48 +0200

    Added API basic functionallity

--- Vejr App/Vejr App/MainWindow.xaml.cs
+++ Vejr App/Vejr App/MainWindow.xaml.cs
@@ -12,17 +12,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Vejr_App
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e) {
+            string apiKey = "ee9567c7599dc716faa331deb5653851";
+            string city = "København";
+            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+
+            using (HttpClient client = new HttpClient()) {
+                try {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    string filePath = "C:\\Users\\phill\\OneDrive - TECHCOLLEGE\\Programmering\\WPF - Opgaver - Thomas\\Den Store Opgave\\Weather App\\Vejr App\\Vejr App\\api_data.json"; // Sti og filnavn for JSON-filen
+                    // Den Store Opgave\Weather App\Vejr App\Vejr App\api_data.json
+                    string jsonResponse1 = JsonConvert.SerializeObject(responseContent);
+
+                    File.WriteAllText(filePath, jsonResponse1);
+
+                    // Dekodér JSON-svaret
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+                    string cityName = jsonResponse.name;
+                    double temperature = jsonResponse.main.temp;
+                    string weatherDescription = jsonResponse.weather[0].description;
+
+                    // Eksempel på opdatering af UI-elementer
+                    cityNameLabel.Content = cityName;
+                    temperatureLabel.Content = temperature.ToString();
+                    weatherDescriptionLabel.Content = weatherDescription;
+                }
+                catch (HttpRequestException ex) {
+                    MessageBox.Show($"Fejl ved hentning af vejrdata: {ex.Message}");
+                }
+            }
         }
     }
 }
